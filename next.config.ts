d5f42055a1fc6e905298356fdd5c085423e3ba8b
Sharique Chaudhary <khanshariq92213@gmail.com>
2026-01-08 18:36:34 +0000

    feat: Configure turbopack with root directory for optimized build process

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack : {
+    root: "/home/sharique/desktop/mcp-servers-and-cli/mediaproc/web/"
+  }
 };
 
 export default nextConfig;
